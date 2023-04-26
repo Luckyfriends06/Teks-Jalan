98ec1847262290120787395d9a25c9667ae2cd8d
Lucky Friends <93716412+Luckyfriends06@users.noreply.github.com>
2023-04-26 18:13:11 +0700

    Create TeksJalan.py
    Kalian Bisa Bikin Teks Sebanyak-Banyaknya dengan Menambahkan Code 
    
    jalan('ISI TEKS YANG KALIAN MAU'.center(44))

---
+++ TeksJalan.py
@@ -0,0 +1,19 @@
+import sys,time
+
+def jalan(kata):
+
+  for e in kata:
+
+    sys.stdout.write(e)
+
+    sys.stdout.flush()
+
+    time.sleep(0.1)
+
+jalan(' JALAN TEKS BY Luckyz '.center(44))
+
+jalan('WOY ANDI LU MAU TEKA TEKI GAK?'.center(44))
+
+jalan('KALO MAU NIH GW KASIH "ES ES APA YANG TURUN DARI LANGIT?'.center(44))
+
+jalan('COBA JAWAB LAH !!!!'.center(44))
